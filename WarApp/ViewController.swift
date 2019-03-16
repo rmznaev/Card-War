18ee6e0f64a012485ea3c3afa74178d219941531
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-03-16 15:44:03 +0400

    Code cleaned and comments added

--- WarApp/ViewController.swift
+++ WarApp/ViewController.swift
@@ -19,43 +19,59 @@ class ViewController: UIViewController {
     @IBOutlet weak var rightScoreLabel: UILabel!
     
     var leftScore = 0
+    
     var rightScore = 0
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
     
     @IBAction func dealTapped(_ sender: Any) {
         
         // Randomize two numbers
+        
         let leftRandomNumber = arc4random_uniform(13) + 2
+        
         let rightRandomNumber = arc4random_uniform(13) + 2
         
         // Change the image views
+        
         leftImageView.image = UIImage(named: "card\(leftRandomNumber)")
+        
         rightImageView.image = UIImage(named: "card\(rightRandomNumber)")
         
         // Compare the numbers
+        
         if leftRandomNumber > rightRandomNumber {
             
             // Update the score
+            
             leftScore += 1
             
             // Update the label
+            
             leftScoreLabel.text = String(leftScore)
             
         } else if leftRandomNumber < rightRandomNumber {
             
             // Update the score
+            
             rightScore += 1
             
             // Update the label
+            
             rightScoreLabel.text = String(rightScore)
             
         } else if leftRandomNumber == rightRandomNumber {
             
+            // keep the same for now
+            
         }
+        
     }
     
 }
